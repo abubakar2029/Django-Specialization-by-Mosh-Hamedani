23271b2db50eee86039f4917eb57263c4707726e
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2026-03-03 20:55:47 +0500

    ORM Module(04) first half completed

--- apps/playground/urls.py
+++ apps/playground/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('views/', views.dot_com_customers, name='views'), # the name attribute is to use in templates and views
+    path('low_inventory/', views.low_inventory_products, name='low_inventory'), # the name attribute is to use in templates and views
+    path('collection_3/', views.collection_3_order_items, name='collection_3_order_items'), # the name attribute is to use in templates and views
+    path('defer_products/', views.defer_products, name='defer_products'), 
+    path('ordered_items/', views.ordered_items, name='ordered_items'), # the name attribute is to use in templates and views
     path('hello/', views.hello_01, name='hello'),
     path('hello_02/', views.hello_02, name='hello_02'),
     path('hello_03/', views.hello_03, name='hello_03'),
--- apps/playground/views.py
+++ apps/playground/views.py
@@ -1,14 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apps.storefront.models import Customer, OrderItem, Product
 
-# Create your views here.
-def hello_01(requests):
+
+# VIEWS Exercises
+def collection_3_order_items(request):
+    query_set = OrderItem.objects.filter(product__collection_id=3).values(
+        'id', 'product__title', 'product__collection__title')
+
+    return render(request, 'collection_3_order_items.html', {'products': query_set})
+
+def ordered_items(request):
+    query_set = OrderItem.objects.all().order_by('product__title').values(
+        'product__id', 'product__title','product__unit_price').distinct()
+    # using disctinct() because on product can appear in multiple order items
+    return render(request, 'ordered_items.html', {'products': query_set})
+
+def defer_products(request):
+    query_set = Product.objects.only(
+        'id', 'description', 'unit_price'
+    ).order_by('id')
+
+    return render(request, 'defer_products.html', {'products': query_set})
+
+def low_inventory_products(request):
+    query_set = Product.objects.filter(inventory__lte=10)
+
+    return render(request, 'low_inventory_products.html', {'products': query_set})
+
+
+def dot_com_customers(request):
+    query_set = Customer.objects.filter(email__endswith='.com')
+
+    return render(request, 'dot_com_customers.html', {'customers': query_set})
+
+
+# HttpRequest raw content (string, HTML, JSON, etc.)
+def hello_01(request):
     return HttpResponse("I am revising Django Concepts!")
 
+
 def hello_02(request):
-    return render(request, 'hello.html', {'name': 'Abubakar'}) 
+    return render(request, 'hello.html', {'name': 'Abubakar'})
 
 # For Debugging Purpose
+
+
 def hello_03(request):
     x = 2
     y = 3
